1325a1c5811efce5cf2f7466a5d939d20b8824b4
C.S Wolf <37349534+CSneed7@users.noreply.github.com>
2018-08-07 19:38:51 -0400

    added files

--- server.js
+++ server.js
@@ -1,47 +1,31 @@
-// Require/import the HTTP module
 var http = require("http");
 
-// Define a port to listen for incoming requests
-var PORT = 7000;
+var PORT1 = 7000;
 
-// Create a generic function to handle requests and responses
 function handleRequest(request, response) {
 
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("You gon` be somebody someday..");
 }
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+server.listen(PORT1, function() {
 
-  // Log (server-side) when our server has started
-  console.log("You gon` be somebody someday..");
+  console.log("You gon` be somebody someday.." + PORT);
 });
 
-// Require/import the HTTP module
 var http = require("http");
 
-// Define a port to listen for incoming requests
 var PORT2 = 7500;
 
-// Create a generic function to handle requests and responses
 function handleRequest(request, response) {
 
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("Yo life sucks, yo.");
 }
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
 
-// Start our server so that it can begin listening to client requests.
 server.listen(PORT2, function() {
 
-  // Log (server-side) when our server has started
   console.log("Yo life sucks, yo.");
 });
